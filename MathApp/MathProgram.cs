9e3ef7eeb597ba611781267eda5c48b519e1c094
ImNatHarvey <jharveycrisologo@gmail.com>
2026-02-07 15:42:51 +0800

    Added Math.abs

--- MathApp/MathProgram.cs
+++ MathApp/MathProgram.cs
@@ -8,9 +8,16 @@ namespace MathApp
 {
     internal class MathProgram
     {
-        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Math Class ---\n");
 
+            // 1. Math.Abs()
+            int negativeNumber = -25;
+            int absoluteResult = Math.Abs(negativeNumber);
+            Console.WriteLine($"1. Absolute value of {negativeNumber} is: {absoluteResult}");
 
-
+            Console.ReadLine();
+        }
     }
 }
